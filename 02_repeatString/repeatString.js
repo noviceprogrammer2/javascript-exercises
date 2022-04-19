95b7d8b154f07ef867cafab2fc6d0524db7ba129
noviceprogrammer2 <simonsbrian91@gmail.com>
2022-04-19 00:36:29 -0400

    completed exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string,num) {
+    newstring = ''; //creates empty string
+    if (num<0){
+        newstring = 'ERROR'; //returns error for negative num values 
+    }
+    else{
+        for (let i = 0; i < num; i++){ 
+            newstring = newstring + string;  //adds value to newstring
+        }
+    }
+    return newstring // once loops are finished
+}
 
 // Do not edit below this line
 module.exports = repeatString;
