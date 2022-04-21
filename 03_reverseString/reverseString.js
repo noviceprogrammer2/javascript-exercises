f5991e7dd8fa3db9ae51e7f142ae47b4b13aef67
noviceprogrammer2 <simonsbrian91@gmail.com>
2022-04-21 14:10:58 -0400

    finished exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    // creates array with string charcters as individual elements 
+    let characters = string.split("");
+    //reverses characters in the array 
+    let reverseArray = characters.reverse();
+    //joins characters in the array 
+    let reverseString = reverseArray.join('');
+    
+    return reverseString;
 };
 
 // Do not edit below this line
