ca8a71ca33b0e713a5de78f0a950fced3427cf86
noviceprogrammer2 <yobo520@gmail.com>
2022-04-23 13:29:43 -0400

    finished exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
 const removeFromArray = function() {
 
+//if value in array, remove value
+//i = 1 to start after first index of input, first input is always an array 
+for (let i = 1; i <= arguments.length ; i++){
+    if (arguments[0].includes(arguments[i]) == true){
+        index = arguments[0].indexOf(arguments[i]); //finds index of array value
+        arguments[0].splice(index, 1); //splices array at value 
+
+    }
+}
+return arguments[0];
 };
 
 // Do not edit below this line
