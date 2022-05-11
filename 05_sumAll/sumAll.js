50358ede454748f16044f2e07fd3313b4f69ccbd
noviceprogrammer2 <yobo520@gmail.com>
2022-05-11 11:50:40 -0400

    finished sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
-
+const sumAll = function (input1, input2) {
+    sum = 0
+    if (typeof (input1) == "string" || typeof (input2) == "string") {
+        return "ERROR"
+    }
+    if (input1 < 0 || input2 < 0) {
+        return "ERROR"
+    }
+    if (typeof (input1) == "number" && typeof (input2) == "number") {
+        //for larger numbers first 
+        if (input1 > input2) {
+            for (let i = input1; i >= input2; i--) {
+                sum = i + sum
+            }
+            return sum
+        }
+        // for larger numbers second 
+        if (input1 < input2) {
+            for (let i = input1; i <= input2; i++) {
+                sum = i + sum
+            }
+            return sum
+        }
+    }
+    else {
+        return "ERROR"
+    }
 };
 
 // Do not edit below this line
